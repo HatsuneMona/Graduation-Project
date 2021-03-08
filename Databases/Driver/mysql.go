f725a59125782f0cbcd66cbc1e31f5e0dc732a08
HatsuneMona <songmiao39@foxmail.com>
2021-03-08 22:50:34 +0800

    MySQL Connect and Test

--- Databases/Driver/mysql.go
+++ Databases/Driver/mysql.go
@@ -3,15 +3,17 @@
  * @Date  2021-02-05 17:05
  * @Description 初始化 MySQL database
  **/
-package MySQL
+package Driver
 
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	_ "gorm.io/driver/mysql"
 )
 
 type connInfo struct {
 	addr     string
+	port     int
 	user     string
 	pw       string
 	database string
@@ -22,18 +24,19 @@ var DB *gorm.DB
 func init() {
 	c := connInfo{
 		addr:     "172.20.0.2",
+		port:     3306,
 		user:     "hospital",
 		pw:       "62QppvxZjCGm7c9jdUyQ",
 		database: "hospital",
 	}
-	connString := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
-		c.user, c.pw, c.addr, c.database)
+	connString := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+		c.user, c.pw, c.addr, c.port, c.database)
 	var err error
 	DB, err = gorm.Open("mysql", connString)
 	if err != nil {
-		fmt.Printf("mysql connect error %v", err)
+		fmt.Printf("mysql connect error %v\n", err)
 	}
 	if DB.Error != nil {
-		fmt.Printf("database error %v", DB.Error)
+		fmt.Printf("database error %v\n", DB.Error)
 	}
 }
--- Test/Databases/MySQL_test.go
+++ Test/Databases/MySQL_test.go
@@ -3,14 +3,17 @@
  * @Date  2021-03-08 20:13
  * @Description //TODO
  **/
-package MySQL
+package Databases
 
 import (
+	"service/Databases/Driver"
 	"testing"
 )
 
 func Test_MySQLConnection(t *testing.T) {
 	t.Run("MySQL连接测试", func(t *testing.T) {
-
+		if err := Driver.DB.DB().Ping(); err != nil {
+			t.Fatalf("%v失败，Ping失败，错误信息：%v", t.Name(), err)
+		}
 	})
 }
