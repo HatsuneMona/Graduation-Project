b798f04f098df19e8488a39e58f9fdb4f2287104
HatsuneMona <songmiao39@foxmail.com>
2021-03-09 10:22:53 +0800

    log模块及其测试

---
+++ Test/logInit_test.go
@@ -0,0 +1,21 @@
+/**
+ * @Author HatsuneMona
+ * @Date  2021-03-09 08:40
+ * @Description 测试log模块
+ **/
+package Test
+
+import (
+	"service/Utils/log"
+	"testing"
+)
+
+func Test_LoggerInit(t *testing.T) {
+	t.Run("测试logger初始化情况", func(t *testing.T) {
+		log.InitLogger()
+		defer log.Logger.Sync()
+		log.Logger.Info("LoggerInfo")
+		log.Logger.Fatal("LoggerFatal")
+		log.Logger.Error("LoggerError")
+	})
+}
---
+++ Utils/log/logInit.go
@@ -0,0 +1,52 @@
+/**
+ * @Author HatsuneMona
+ * @Date  2021-03-09 08:26
+ * @Description 初始化log相关内容
+ **/
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var Logger *zap.Logger
+var SugarLogger *zap.SugaredLogger
+
+type logInfo struct {
+	logFile string //指定日志文件
+	debug   bool   //true：输出日志到控制台；false：输出日志到文件
+}
+
+var conf logInfo = logInfo{
+	logFile: "./test.log",
+	debug:   true,
+}
+
+func init() {
+	InitLogger()
+}
+
+func InitLogger() {
+	if conf.debug == true {
+		Logger = zap.NewExample()
+		SugarLogger = Logger.Sugar()
+	} else {
+		writeSyncer := getLogWriter(conf.logFile)
+		encoder := getEncoder()
+		core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+		Logger = zap.New(core)
+		SugarLogger = Logger.Sugar()
+	}
+}
+
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+}
+
+func getLogWriter(filePath string) zapcore.WriteSyncer {
+	file, _ := os.Create(filePath)
+	return zapcore.AddSync(file)
+}
