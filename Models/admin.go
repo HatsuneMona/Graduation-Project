ebecf8aaa7166063aede64ada93dbb0f808a2841
HatsuneMona <songmiao39@foxmail.com>
2021-03-10 20:44:40 +0800

    admin database Handler v1.2

--- Models/admin.go
+++ Models/admin.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Admin struct {
-	AdminID           int    `json:"admin_id" gorm:"primary_key;column:admin_id;type:int(11) auto_increment;comment:'管理员id'"`                                                                      // 管理员id
-	AdminUsername     string `json:"admin_username" gorm:"column:admin_username;type:char(20);not null;unique_index:hospital_admin_admin_username_uindex;comment:'管理员用户名'"`                        // 管理员用户名
-	AdminPassword     string `json:"admin_password" gorm:"column:admin_password;type:char(30);not null;comment:'管理员密码'"`                                                                           // 管理员密码
-	AdminName         string `json:"admin_name" gorm:"column:admin_name;type:varchar(10);not null;comment:'管理员名字'"`                                                                                // 管理员名字
-	AdminPhonenum     string `json:"admin_phoneNum" gorm:"column:admin_phoneNum;type:char(15);not null;comment:'管理员电话'"`                                                                           // 管理员电话
-	AdminPermissionID int    `json:"admin_permission_id" gorm:"column:admin_permission_id;type:int(11);not null;index:hospital_admin_hospital_admin_permissions_permission_id_fk;comment:'管理员权限'"` // 管理员权限
+	ID           int    `json:"admin_id" gorm:"primary_key;column:admin_id;type:int(11) auto_increment;comment:'管理员id'"`                                                                      // 管理员id
+	Username     string `json:"admin_username" gorm:"column:admin_username;type:char(20);not null;unique_index:hospital_admin_admin_username_uindex;comment:'管理员用户名'"`                        // 管理员用户名
+	Password     string `json:"admin_password" gorm:"column:admin_password;type:char(30);not null;comment:'管理员密码'"`                                                                           // 管理员密码
+	Name         string `json:"admin_name" gorm:"column:admin_name;type:varchar(10);not null;comment:'管理员名字'"`                                                                                // 管理员名字
+	Phonenum     string `json:"admin_phoneNum" gorm:"column:admin_phoneNum;type:char(15);not null;comment:'管理员电话'"`                                                                           // 管理员电话
+	PermissionID int    `json:"admin_permission_id" gorm:"column:admin_permission_id;type:int(11);not null;index:hospital_admin_hospital_admin_permissions_permission_id_fk;comment:'管理员权限'"` // 管理员权限
 	AdminHandlers
 }
 
@@ -21,6 +21,11 @@ func (admin *Admin) TableName() string {
 	return "hospital_admin"
 }
 
+/*
+ * @description   关于Admin操作的相关接口，其中包括读取（查询）Admin信息接口，设置（更新、添加、删除）Admin信息接口）
+ * @auther        HatsuneMona
+ * @createTime    2021/3/10 16:45
+ */
 type AdminHandlers interface {
 	adminReader
 	adminSetter
@@ -36,11 +41,20 @@ type adminSetter interface {
 	UpdateName(newName string) error
 	UpdatePhonenum(newPhoneNum string) error
 	UpdatePermissionID(newPermissionID int) error
+	AddNewAdmin() error
+	Delete() error
 }
 
+/*
+ * @description   通过给定adminID，从数据库中查询admin信息
+ * @param         id int 需要查询的adminID
+ * @return        查询成功：error=nil，失败则返回报错信息。
+ * @auther        HatsuneMona
+ * @createTime    2021/3/10 17:31
+ */
 func (admin *Admin) GetAdminByID(id int) error {
-	admin.AdminID = id
-	res := DBPool.DB.First(&admin)
+	admin.ID = id
+	res := DBPool.DB.Take(&admin)
 	//if res.Value != 1 {
 	//	log.Logger.Info(fmt.Sprintf("查询Admin错误，查询结果数量=%v", res.Value))
 	//	return admin, errors.New(fmt.Sprintf("查询Admin错误，查询结果数量=%v", res.Value))
@@ -52,9 +66,16 @@ func (admin *Admin) GetAdminByID(id int) error {
 	return nil
 }
 
+/*
+ * @description   通过给定adminID，从数据库中查询admin信息
+ * @param         id int 需要查询的adminID
+ * @return        查询成功：error=nil，失败则返回报错信息。
+ * @auther        HatsuneMona
+ * @createTime    2021/3/10 17:31
+ */
 func (admin *Admin) GetAdminByUsername(username string) error {
-	res := DBPool.DB.Where("admin_username = ?", username).First(&admin)
-	//if res.Value != 1 {
+	res := DBPool.DB.Where("admin_username = ?", username).Take(&admin)
+	// if res.Value != 1 {
 	//	log.Logger.Info(fmt.Sprintf("查询Admin错误，查询结果数量=%v", res.Value))
 	//	return admin, errors.New(fmt.Sprintf("查询Admin错误，查询结果数量=%v", res.Value))
 	//}
@@ -65,8 +86,23 @@ func (admin *Admin) GetAdminByUsername(username string) error {
 	return nil
 }
 
+/*
+@Description	更新Admin用户密码
+
+@Param
+
+`newPassword` string 新密码
+
+@Return
+
+成功：error=nil，失败：返回错误信息。
+
+@Author        HatsuneMona
+
+@CreateTime    2021/3/10 17:29
+*/
 func (admin *Admin) UpdatePassword(newPassword string) error {
-	if admin.AdminPassword == newPassword {
+	if admin.Password == newPassword {
 		return errors.New("新密码与旧密码相同，pass")
 	}
 	result := DBPool.DB.Model(&admin).Update("admin_password", newPassword)
@@ -77,7 +113,7 @@ func (admin *Admin) UpdatePassword(newPassword string) error {
 }
 
 func (admin *Admin) UpdateName(newName string) error {
-	if admin.AdminName == newName {
+	if admin.Name == newName {
 		return errors.New("新名字与旧名字相同，pass")
 	}
 	result := DBPool.DB.Model(&admin).Update("admin_name", newName)
@@ -88,7 +124,7 @@ func (admin *Admin) UpdateName(newName string) error {
 }
 
 func (admin *Admin) UpdatePhonenum(newPhoneNum string) error {
-	if admin.AdminPhonenum == newPhoneNum {
+	if admin.Phonenum == newPhoneNum {
 		return errors.New("新手机号与旧手机号相同，pass")
 	}
 	result := DBPool.DB.Model(&admin).Update("admin_phonenum", newPhoneNum)
@@ -99,12 +135,47 @@ func (admin *Admin) UpdatePhonenum(newPhoneNum string) error {
 }
 
 func (admin *Admin) UpdatePermissionID(newPermissionID int) error {
-	if admin.AdminPermissionID == newPermissionID {
+	if admin.PermissionID == newPermissionID {
 		return errors.New("新权限与旧权限相同，pass")
 	}
 	result := DBPool.DB.Model(&admin).Update("admin_permission_id", newPermissionID)
 	if result.Error != nil {
 		return result.Error
+	}
+	return nil
+}
+
+func (admin *Admin) AddNewAdmin() error {
+	if admin.ID != 0 {
+		return errors.New("禁止指定AdminID")
+	}
+	result := DBPool.DB.Create(&admin)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (admin *Admin) Delete() error {
+	if admin.ID == 0 {
+		return errors.New("请提供正确的adminID")
+	}
+	result := DBPool.DB.Delete(&admin)
+	if result.RowsAffected == 0 {
+		return errors.New(fmt.Sprintf("删除失败，查无此ID（DeleteID=%v）", admin.ID))
+	}
+	if result.Error != nil {
+		return result.Error
+	}
+	//原有数据是否需要手动覆盖？需要。
+	*admin = Admin{
+		ID:            -admin.ID,
+		Username:      "(Deleted)",
+		Password:      "(Deleted)",
+		Name:          "(Deleted)",
+		Phonenum:      "(Deleted)",
+		PermissionID:  -1, //系统保留权限值，空权限
+		AdminHandlers: nil,
 	}
 	return nil
 }
--- Test/Models_test/AdminHandler_test.go
+++ Test/Models_test/AdminHandler_test.go
@@ -17,8 +17,19 @@ func Test_AdminReader(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v错误，错误信息：%v", t.Name(), err)
 		}
-		if admin.AdminUsername != "TestAdmin" {
-			t.Fatalf("%v错误，错误信息：预期AdminName值不正确=%v", t.Name(), admin.AdminUsername)
+		if admin.Username != "TestAdmin" {
+			t.Fatalf("%v错误，错误信息：预期AdminName值不正确=%v", t.Name(), admin.Username)
+		}
+		t.Log(admin)
+	})
+
+	t.Run("测试GetAdminByID，无结果（错误结果测试）", func(t *testing.T) {
+		admin := new(Models.Admin)
+		err := admin.GetAdminByID(9900)
+		if err != nil {
+			t.Logf("%v得到原函数传来的，错误信息：%v", t.Name(), err)
+		} else {
+			t.Fatalf("未得到错误，测试未通过。")
 		}
 		t.Log(admin)
 	})
@@ -29,13 +40,54 @@ func Test_AdminReader(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%v错误，错误信息：%v", t.Name(), err)
 		}
-		if admin.AdminID != 9999 {
-			t.Fatalf("%v错误，错误信息：预期AdminID值不正确=%v", t.Name(), admin.AdminID)
+		if admin.ID != 9999 {
+			t.Fatalf("%v错误，错误信息：预期AdminID值不正确=%v", t.Name(), admin.ID)
 		}
 		t.Log(admin)
 	})
 }
 
 func Test_AdminSetter(t *testing.T) {
-	//略
+	newAdmin := Models.Admin{
+		ID:            0,
+		Username:      "TestAddAdmin",
+		Password:      "TestAddAdmin",
+		Name:          "添加测试",
+		Phonenum:      "TestAddAdmin",
+		PermissionID:  -2,
+		AdminHandlers: nil,
+	}
+
+	t.Run("添加、删除用户测试（添加成功测试）", func(t *testing.T) {
+		err := newAdmin.AddNewAdmin()
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			t.Logf("添加成功后，newAdmin信息如下：%v", newAdmin)
+		}
+		err = newAdmin.Delete()
+		if err != nil {
+			t.Fatal(err)
+		} else {
+			t.Logf("删除成功后，newAdmin信息如下：%v", newAdmin)
+		}
+	})
+
+	t.Run("删除失败", func(t *testing.T) {
+		deleteErrAdmin := Models.Admin{
+			ID:            9000,
+			Username:      "nothing",
+			Password:      "nothing",
+			Name:          "nothing",
+			Phonenum:      "nothing",
+			PermissionID:  -2,
+			AdminHandlers: nil,
+		}
+		err := deleteErrAdmin.Delete()
+		if err == nil {
+			t.Fatal("删除不存在的数据时未报错")
+		} else {
+			t.Logf("删除不存在的数据时成功报错，报错信息：%v", err)
+		}
+	})
 }
