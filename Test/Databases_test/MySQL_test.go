a749b92404f2a99fa4507a7040161855b76501d9
HatsuneMona <songmiao39@foxmail.com>
2021-03-13 18:46:57 +0800

    MySQL_test.go

--- Test/Databases_test/MySQL_test.go
+++ Test/Databases_test/MySQL_test.go
@@ -6,13 +6,13 @@
 package Databases_test
 
 import (
-	"service/Databases/DBPool"
+	"service/Databases"
 	"testing"
 )
 
 func Test_MySQLConnection(t *testing.T) {
 	t.Run("MySQL连接测试", func(t *testing.T) {
-		if err := DBPool.DB.DB().Ping(); err != nil {
+		if err := Databases.DB.DB().Ping(); err != nil {
 			t.Fatalf("%v失败，Ping失败，错误信息：%v", t.Name(), err)
 		}
 	})
