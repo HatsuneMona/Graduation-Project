b8e3c8a46526a9d4633b616103a1e10b7c834252
HatsuneMona <songmiao39@foxmail.com>
2021-03-14 10:41:04 +0800

    Redis 连接初始化、及其测试

--- Databases/redis.go
+++ Databases/redis.go
@@ -4,3 +4,27 @@
  * @Description 初始化 Redis
  **/
 package Databases
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+var RDB *redis.Client
+
+func init() {
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     "172.20.0.2:6379",
+		Password: "AHzkfP8ViCX&5MPT5x$VDUjeKWHv6uM4^&Q2qfY9FB0qysuo*@JGbyil7Qa%t7mN", //随机生成的，你看到了也没有用
+		DB:       0,
+	})
+	ctx := context.Background()
+	str, err := RDB.Ping(ctx).Result()
+	if err != nil {
+		fmt.Printf("Redis连接错误，错误信息：%v\n", err)
+		fmt.Printf("Redis连接错误，Ping信息：%v\n", str)
+	} else {
+		fmt.Printf("Redis连接成功，ping信息：%v\n", str)
+	}
+}
---
+++ Test/Databases_test/redis_test.go
@@ -0,0 +1,25 @@
+// @Title       Databases
+// @Description 文件描述
+// @Author      HatsuneMona
+// @CreateTime  2021/3/13 22:24
+
+package Databases
+
+import (
+	"context"
+	"fmt"
+	"service/Databases"
+	"testing"
+)
+
+func Test_RedisConnection(t *testing.T) {
+	t.Run("Redis连接测试", func(t *testing.T) {
+		ctx := context.Background()
+		str, err := Databases.RDB.Ping(ctx).Result()
+		if err != nil {
+			t.Fatal(fmt.Sprintf("Redis连接错误，错误信息：%v\n", err))
+		} else {
+			t.Logf("Redis连接成功，ping信息：%v\n", str)
+		}
+	})
+}
